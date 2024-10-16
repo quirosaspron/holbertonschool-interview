2446866a5976d155a3c91c0986b3fcaf448d5a12
Mateo <mateoquirosaspron@gmail.com>
2024-10-15 22:04:24 -0600

    -

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -8,30 +8,35 @@
  * Return: address of the new node or NULL if it fails
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL){
-        free(new_node);
-        return (NULL);
-    }
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (parent){ 
-        if (parent->left){ // if parent already has a left child
-            if (parent->right){ // if parent also has a right child
-                free(new_node);
-                return (NULL);
-            }
-            else // there is no right child
-                parent->right = new_node;
-       }
-       else // add to the left
-           parent->left = new_node;
-   }
-   new_node->left = NULL;
-   new_node->right = NULL;
-   new_node->n = value;
-   new_node->parent = parent;
+	if (parent)
+	{
+		if (parent->left)
+		{
+			if (parent->right)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		else
+			parent->right = new_node;
+		}
+	else
+		parent->left = new_node;
+	}
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
 
-   return (new_node);
+	return (new_node);
 }
